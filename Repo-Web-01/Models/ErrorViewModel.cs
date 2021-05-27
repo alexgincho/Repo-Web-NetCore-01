bf53c42d5cab510dc0ab0581c2dca477296dc152
Alexander Ch. G <ginchoalexch@gmail.com>
2021-05-26 23:50:37 -0500

    Empezando el Proyecto Vacio

---
+++ Repo-Web-01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Repo_Web_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
